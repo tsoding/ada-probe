f35253c96c1ea28dfa930f453ddc206c5651db96
OneWingedShark <OneWingedShark@gmail.com>
2021-01-13 10:35:56 -0700

    Added some comments.

--- src/aids-env.adb
+++ src/aids-env.adb
@@ -13,9 +13,9 @@ package body Aids.Env is
 
     function Index_Of(Line: in String; X: in Character; Result: out Integer) return Boolean is
         Index : Natural renames Ada.strings.Fixed.Index(
-           Source  => Line,
-           Pattern => (1 => X),
-           From    => Line'First
+           Source  => Line,      -- The String we're searching,
+           Pattern => (1 => X),  -- the *Substring* to search for;
+           From    => Line'First -- the point we start from.
           );
     begin
         Return Found : constant Boolean := Index in Positive do
@@ -24,48 +24,49 @@ package body Aids.Env is
     end;
 
     function Slurp(File_Path: String) return Env_Hashed_Map.Map is
-        Result: Env_Hashed_Map.Map;
         File: File_Type;
         Line_Number: Integer := 1;
     begin
-        Open(File => File,
-             Mode => In_File,
-             Name => File_Path);
-        while not End_Of_File(File) loop
-            declare
-                Line: String := Get_Line(File);
-                Index : Integer;
-            begin
-                if Index_Of(Line, '=', Index) then
-                    declare
-                        Prev  : Natural  renames Positive'Pred( Index );
-                        Next  : Positive renames Positive'Succ( index );
+        Return Result: Env_Hashed_Map.Map do
+            Open(File => File,
+                 Mode => In_File,
+                 Name => File_Path);
+            while not End_Of_File(File) loop
+                declare
+                    Line: String := Get_Line(File);
+                    Index : Integer;
+                begin
+                    if Index_Of(Line, '=', Index) then
+                        declare
+                            Use Ada.Strings;
+                            Prev  : Natural  renames Positive'Pred( Index );
+                            Next  : Positive renames Positive'Succ( index );
                         
-                        -- Rename the Trim-function, and give it a default of
-                        -- both sides; this handles a line like:
-                        -- "   This_key      =     Some_Value "
-                        Function Trim(Object : String;
-                                      Sides  : Ada.Strings.Trim_End:= Ada.Strings.Both
-                                     ) return String
-                           renames Ada.Strings.Fixed.Trim;
+                            -- Rename the Trim-function, and give it default for
+                            -- "both sides"; this handles a line like:
+                            -- "   This_key      =     Some_Value "
+                            Function Trim(Object : String;
+                                          Sides  : Trim_End:= Both
+                                         ) return String
+                                      renames Fixed.Trim;
                           
-                        Key   : String renames Trim( Line(Line'First..Prev) );
-                        Value : String renames Trim( Line(Next..Line'Last)  );
-                    begin
-                        Env_Hashed_Map.Insert(Result, Key, Value);
-                    end;
-                else
-                    raise Syntax_Error with (File_Path 
-                                             & ":" 
-                                             & Integer'Image(Line_Number)
-                                             & ": Expected separator `=`");
-                end if;
+                            Key   : String renames Trim(Line(Line'First..Prev));
+                            Value : String renames Trim(Line(Next..Line'Last) );
+                        begin
+                            Env_Hashed_Map.Insert(Result, Key, Value);
+                        end;
+                    else
+                        raise Syntax_Error with (File_Path 
+                                                 & ":" 
+                                                 & Integer'Image(Line_Number)
+                                                 & ": Expected separator `=`");
+                    end if;
 
-                Line_Number := Line_Number + 1;
-            end;
-        end loop;
-        Close(File);
-        return Result;
+                    Line_Number := Line_Number + 1;
+                end;
+            end loop;
+            Close(File);
+        End return;
     end;
 
     function Find(Env: in Typ; Key: in Unbounded_String; Value: out Unbounded_String) return Boolean is 
